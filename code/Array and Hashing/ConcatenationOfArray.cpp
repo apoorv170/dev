ab49bc3a54cae0a4f917f80c099bb66960cae949
apoorv170 <gsoc.2019@gmail.com>
2024-12-15 10:47:20 +0000

    concatArray

---
+++ code/Array and Hashing/ConcatenationOfArray.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> concatArray (vector<int>& nums) {
+    vector<int> ans;
+    for (int i = 0 ; i < nums.size() ; i++) {
+        ans.push_back(nums[i]);
+    }
+    for (int i = 0 ; i < nums.size() ; i++) {
+        ans.push_back(nums[i]);
+    }
+    return ans;
+}
+
+
+int main() {
+    vector<int> nums{1,2,1};
+    vector<int> ans = concatArray(nums);
+    for( auto x : ans) {
+        cout << x << " ";
+    }
+    cout << endl;
+    return 0;
+}
